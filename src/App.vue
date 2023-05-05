58f93c62e1049efdc7f9ad57526e70bd5b15061a
David Saldua <davidsaldua24@gmail.com>
2023-05-06 00:49:04 +0800

    add files

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+        <div id="app">
+                <h1>Todo-List</h1>
+                <ul>
+                        <li>
+                                 <TodoItem label="My Todo Item"  :done="false" ></TodoItem> 
+                        </li>
+                </ul>
+        </div>
+  
+  
+</template>
+
+<script>
+import TodoItem from "./components/ToDoItem.vue";
+
+
+
+export default {
+  name: 'App',
+  components: {
+        TodoItem,
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
